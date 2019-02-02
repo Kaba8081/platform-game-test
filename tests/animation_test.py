4a6d745fc8ee12c63964b08a8d1a5346b3909d59
Kaba <Kaba8081@gmail.com>
2019-02-02 14:23:07 +0100

    0.0.0.1.1
    some 'animation_test.py' changes

--- tests/animation_test.py
+++ tests/animation_test.py
@@ -1,17 +1,37 @@
 from os import path
 
-def animations(mob,state,speed):
+def animations(mob,state,direction,background,speed):
     import pygame
     
     pygame.init()
-    screen = pygame.display.set_mode((200,200))
+    screen = pygame.display.set_mode((192,192))
     allSprites = pygame.sprite.Group()
     clock = pygame.time.Clock()
 
     done = False
     textures = []
-    for i in range(2):
-        textures.append(pygame.transform.scale(pygame.image.load(path.join(imgDir,'{0}_{1}{2}.png'.format(mob,state,i+1))).convert_alpha(),(64,64)))
+    if direction == 'L':
+        for i in range(2):
+            textures.append(pygame.transform.scale(pygame.image.load(path.join(imgDir,'{0}_{1}{2}.png'.format(mob,state,i+1))).convert_alpha(),(64,64)))
+    elif direction == 'R':
+        for i in range(2):
+            textures.append(pygame.transform.flip(pygame.transform.scale(pygame.image.load(path.join(imgDir,'{0}_{1}{2}.png'.format(mob,state,i+1))).convert_alpha(),(64,64)), True, False))
+    
+    class background_sprite(pygame.sprite.Sprite):
+        def __init__(self,texture,x):
+            pygame.sprite.Sprite.__init__(self)
+
+            self.image = texture
+            self.rect = pygame.Rect(x,136,64,64)
+
+    background_color = (0,0,0)
+    if background == '':
+        pass
+    elif background == 'overworld':
+        background_color = (20, 198, 229)
+        for i in range(3):
+            bs = background_sprite(pygame.transform.scale(pygame.image.load(path.join(path.dirname(__file__),'../textures/ground/dirt2.png')).convert_alpha(),(64,64)),i*64)
+            allSprites.add(bs)
 
     class sprite(pygame.sprite.Sprite):
         def __init__(self,textures):
@@ -21,7 +41,7 @@ def animations(mob,state,speed):
             self.index = 0
             self.tick = 0
             self.image = self.images[self.index]
-            self.rect = pygame.Rect(5,5,64,64)    
+            self.rect = pygame.Rect(72,72,64,64)    
         def update(self):
             if self.tick >= 61:
                 self.index += 1
@@ -40,18 +60,24 @@ def animations(mob,state,speed):
                 pygame.quit()
                 done = True
                 return
-        screen.fill((0,0,0))
+        screen.fill(background_color)
         allSprites.update()
         allSprites.draw(screen)
         pygame.display.flip()
         clock.tick(60)
 
 while True:
+    print("W jakim folderze jest ta animacja?")
+    folder = input()
+
+    imgDir = path.join(path.dirname(__file__),'../textures/'+folder)
     print("Czego animację mam wyświetlić?")
     mob = input()
     print("Jaki stan tej postaci mam wyświetlić?")
     state = input()
+    print("W którą stronę postać ma się patrzyć? (L lub R)")
+    direction = input()
+    print("Jakie tło ma mieć symulacja? (dla oryginalnego, zostaw puste)")
+    background = input()
 
-    imgDir = path.join(path.dirname(__file__),'../textures/mobs')
-
-    animations(mob,state,1)
+    animations(mob,state,direction,background,1)
